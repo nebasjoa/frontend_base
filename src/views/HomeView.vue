c4cb4826613c08c7690a8748ad2f10d6d2ca33cc
Nebojsa Pesic <nebasjoa@gmail.com>
2025-11-25 22:28:57 +0100

    added check method

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
 <template>
     <h1>Home View</h1>
+    <button @click="check">Check health</button>
 </template>
 
-<script></script>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        async check() {
+            const response = await axios.get('/health');
+            console.log(response)
+        }
+    }
+}
+</script>
 
 <style scoped></style>
