fb24c6fdebcb9203f0a0921ddbb16ca6cff774bd
Nebojsa Pesic <nebasjoa@gmail.com>
2025-12-31 00:48:21 +0100

    displaying response from backend on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
     <h1>Home View</h1>
     <button @click="check">Check health</button>
+    <div>{{  }}</div>
 </template>
 
 <script>
@@ -8,16 +9,18 @@ import axios from 'axios';
 export default {
     data() {
         return {
-
+            response: null
         }
     },
     methods: {
         async check() {
             const response = await axios.get('/health');
+            this.response = response
             console.log(response)
         }
     }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
